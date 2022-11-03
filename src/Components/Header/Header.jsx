bd54e274ded92db530176430aca13a71ed7a1572
SusanaHueso <susana.huesodevis@xing.com>
2022-11-03 14:22:08 +0100

    useNavigate instead of navLink, useEffect, useState

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import './Header.css'
-import { NavLink} from 'react-router-dom';
+// best practice!
+import {useNavigate} from 'react-router-dom';
+
 
 const Header = () =>{
-    return ( <nav>
-        <NavLink  exact="true" to="/">
-            Home 
-        </NavLink> 
-            {" | "}
-        <NavLink  to="/about" > 
-            About
-        </NavLink> 
-    </nav>)
-}
+    let navigate = useNavigate();
+
+    return (  
+        <div className="headerDesign">
+            <div className="linkDesign" onClick={()=>setTimeout(()=>{navigate("/login")},750)}>Login</div>
+            <div className="linkDesign" onClick={()=>setTimeout(()=>{navigate("/register")},750)}>Register</div>
+            <div className="linkDesign" onClick={()=>setTimeout(()=>{navigate("/")},750)}>Home</div>
+            <div className="linkDesign" onClick={()=>setTimeout(()=>{navigate("/about")},750)}>About</div>
+            <div className="linkDesign" onClick={()=>setTimeout(()=>{navigate("/profile")},750)}>Profile</div>
+    
+        </div>
+)}
 export default Header;
--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
 import './Home.css'
 
 const Home = () =>{
-    return (<div className ="homeDesign"> Home</div>)
+    const [toggle,setToggle] = useState(0)
+    // UseEffect will change value of Toggle at mounting
+    // useEffect(()=>{setToggle(1)},[])
+    // UseEffect will change value of Toggle continuously (loop)
+    // useEffect(()=>{setToggle(1)},)    
+    // useEffect(()=>{setToggle(1)},[toggle])
+
+    
+
+    return (<div className ="homeDesign">
+        
+        <button onClick={() => (toggle === 0) ? setToggle(1): setToggle(0)}>This is a toggle button</button> 
+        
+         <p>{toggle}</p></div>)
 }
 export default Home;
