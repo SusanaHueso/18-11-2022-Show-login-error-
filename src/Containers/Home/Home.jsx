28f19fd651c80005441c02c44aee4e57a3a58519
SusanaHueso <susana.huesodevis@xing.com>
2022-11-10 14:03:39 +0100

    added hide button,componentDidMount and componentDidUpdate

--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -1,24 +1,54 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import * as ReactDOM from "react-dom";
 import "./Home.css";
 
 export default class Home extends React.Component {
   // const [toggle,setToggle] = useState(0);
   constructor(props) {
     super(props);
-    this.state = { toggle: 0 };
+    this.state = { show: true, toggle: 0, componentState: "" };
   }
-  //componentWillUnmount(){}
+
+  componentDidMount() {
+    this.setState({ componentState: "only the fist time" });
+    setTimeout(() => {
+      console.log("I appear", this.state.componentState);
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.toggle !== this.state.toggle) {
+      this.setState({ componentState: "everytime the toggle component updates" });
+      setTimeout(() => {
+        console.log("I appear", this.state.componentState);
+      }, 1000);
+    }
+  }
+
   change() {
     let toggleChanged = this.state.toggle === 0 ? 1 : 0;
     this.setState({ toggle: toggleChanged });
+  }
+  hide() {
+    this.state.show === true
+      ? this.setState({ show: false })
+      : this.setState({ show: true });
   }
 
   render() {
     return (
       <div className="homeDesign">
-        <button onClick={() => this.change()}>This is a toggle button</button>
-
-        <p>{this.state.toggle}</p>
+        {this.state.show && (
+          <div>
+            <button onClick={() => this.change()}>
+              This is a toggle button
+            </button>
+            <p>{this.state.toggle}</p>
+          </div>
+        )}
+        <button onClick={() => this.hide()}>
+          Click to hide/show the toggle button
+        </button>
       </div>
     );
   }
